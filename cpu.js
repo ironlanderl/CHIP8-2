be7d953ce4c5f7242c0329e6b04a848e00e5fb58
ironlanderl <fabrizio03p@gmail.com>
2021-12-24 14:03:42 +0100

    Reimplementata la GPU ed alcuni altri opcodes

--- cpu.js
+++ cpu.js
@@ -2,7 +2,7 @@ class CPU{
     // Screen data
     SCREEN_WIDTH = 64;
     SCREEN_HEIGHT = 32;
-    screen = new Uint8Array(this.SCREEN_WIDTH * this.SCREEN_HEIGHT);
+    schermo = this.CreaMatrice();
 
 
     // RAM
@@ -19,6 +19,7 @@ class CPU{
 
     // Stack
     STACK_SIZE = 16;
+    /**@type {Array} */
     stack = new Uint16Array(this.STACK_SIZE);
     // Stack pointer
     SP = 0;
@@ -60,9 +61,15 @@ class CPU{
         }
     }
 
+    /*MainLoop(){
+        this.Fetch();
+        this.Execute();
+    }*/
+
     Fetch(){
         // Split the instruction in two parts
-        this.opcode = this.RAM[this.PC] | this.RAM[this.PC + 1];
+        // La prima parte deve essere spostata a sinistra di 8 bit
+        this.opcode = this.RAM[this.PC] << 8 | this.RAM[this.PC + 1];
         this.PC += 2;
     }
 
@@ -70,13 +77,145 @@ class CPU{
     Execute(){
         // Decode
         let a = (this.opcode & 0xF000) >> 12;
-        let x = (this.opcode & 0x0F00) >> 8;
-        let y = (this.opcode & 0x00F0) >> 4;
-        let n = this.opcode & 0x000F;
-        let nn = this.opcode & 0x00FF;
-        let nnn = this.opcode & 0x0FFF;
-        
+        let X = (this.opcode & 0x0F00) >> 8;
+        let Y = (this.opcode & 0x00F0) >> 4;
+        let N = this.opcode & 0x000F;
+        let NN = this.opcode & 0x00FF;
+        let NNN = this.opcode & 0x0FFF;
+
         // Execute
+
+        switch (a) {
+            case 0:
+                if (this.opcode == 0x00E0) this.screen = new Uint8Array(this.SCREEN_HEIGHT * this.SCREEN_WIDTH)
+                if (this.opcode == 0x00EE){
+                    this.PC = this.stack[this.SP];
+                    this.SP--;
+                }
+                break;
+
+            case 1:
+                this.PC = NNN;
+                break;
+
+            case 2:
+                this.stack[this.SP];
+                this.SP++;
+                this.PC = NNN;
+                break;
+            case 3:
+                if (this.V[X] == NN)
+                {
+                    this.PC += 2;
+                }
+                break;
+            case 4:
+                if (this.V[X] != NN)
+                {
+                    this.PC += 2;
+                }
+                break;
+            case 5:
+                if (this.V[X] == this.V[Y])
+                {
+                    this.PC += 2;
+                }
+                break;
+            case 6:
+                this.V[X] = NN;
+                break;
+            case 7:
+                this.V[X] += NN;
+                break;
+            case 8:
+                switch (N)
+                {
+                    case 0:
+                        this.V[X] = this.V[Y];
+                        break;
+                    case 1:
+                        this.V[X] = this.V[X] | this.V[Y];
+                        break;
+                    case 2:
+                        this.V[X] = this.V[X] & this.V[Y];
+                        break;
+                    case 3:
+                        this.V[X] = this.V[X] ^= this.V[Y];
+                        break;
+                    case 4:
+                        // Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.
+                        this.V[0xF] = (this.V[X] + this.V[Y] > 255) ? 1 : 0;
+                        this.V[X] += this.V[Y];
+                        break;
+                    case 5:
+                        // VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not.
+                        this.V[0xF] = (this.V[X] > this.V[Y]) ? 0 : 1;
+                        this.V[X] -= this.V[Y];
+                        break;
+                    case 6:
+                        // Save LSB
+                        this.V[0xF] = this.V[X] & 1;
+                        // Move V[X] to the right
+                        this.V[X] = this.V[X] >>> 1
+                        break;
+
+                    default:
+                        console.log("Unkown OPCODE: " + this.opcode.toString(16));
+                        break;
+                }
+                break;
+
+
+
+
+
+            case 0xA:
+                this.I = NNN;
+                break;
+
+            case 0xD:
+                //debugger;
+                // Prendo le coordinate dove disegnare
+                let x = this.V[X] % this.SCREEN_WIDTH
+                let y = this.V[Y] % this.SCREEN_HEIGHT
+                // Resetto V[0xF]
+                // Segna se un bit è stato sovrascritto
+                this.V[0xF] = 0
+
+                // altezza
+                for (let i = 0; i < N; i++)
+                {
+                    // prendo i dati per la riga 'i' dello sprite
+                    //let data = this.RAM[this.I + i];
+                    // scrivo allo schermo in base alla lunghezza fissa di 8bit per riga
+                    for (let j = 0; j < 8; j++)
+                    {
+                        // Controllo se esco dallo schermo
+                        if (x + j < this.SCREEN_WIDTH)
+                        {
+                            let data = this.RAM[this.I + i] & (1 << (7 - j)) ? 1 : 0
+                            // controllo se il bit è 1
+                            if (data)
+                            {
+                                if (this.schermo[y + i][x + j] == false)
+                                {
+                                    this.schermo[y + i][x + j] = true;
+                                }
+                                else
+                                {
+                                    this.schermo[y + i][x + j] = false;
+                                    this.V[0xF] = 0x1;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+
+            default:
+                console.log("Unkown OPCODE: " + this.opcode.toString(16));
+                break;
+        }
     }
 
 
@@ -118,8 +257,32 @@ class CPU{
     constructor(ROM){
         this.CaricaROM(ROM);
         this.CaricaFont();
-        debugger;
+        //debugger;
         this.Fetch();
         this.Execute();
     }
+
+    // Create a function that creates a matrix with SCREEN_HEIGHT rows and SCREEN_WIDTH columns
+    // Each cell is a boolean value
+    // The function should return a matrix
+    CreaMatrice(){
+        let tmp = new Array(this.SCREEN_HEIGHT);
+        for (let i = 0; i < this.SCREEN_HEIGHT; i++)
+        {
+            tmp[i] = new Array(this.SCREEN_WIDTH);
+        }
+
+        // Set all the matrix cells to false
+        for (let i = 0; i < this.SCREEN_HEIGHT; i++)
+        {
+            for (let j = 0; j < this.SCREEN_WIDTH; j++)
+            {
+                tmp[i][j] = false;
+            }
+        }
+
+
+        return tmp;
+    }
+
 }
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var ROM = []
 var cpu;
+var animationID
+var canvas;
+/**@type {CanvasRenderingContext2D} */
+var ctx;
+
+const SCREEN_WIDTH = 64;
+const SCREEN_HEIGHT = 32;
+
 
 // Function to load the ROM from the input tag with id "file" to the ROM uint8array
 function CaricaROM() {
@@ -9,6 +17,42 @@ function CaricaROM() {
         var data = reader.result;
         ROM = new Uint8Array(data);
         cpu = new CPU(ROM);
+        setInterval(MainLoop, 1);
+        animationID = window.requestAnimationFrame(DisegnaSchermo);
     };
     reader.readAsArrayBuffer(file);
+}
+
+function MainLoop(){
+    cpu.Fetch();
+    cpu.Execute();
+}
+
+function DisegnaSchermo(){
+    // Prendo lo schermo
+    let schermo = cpu.schermo
+
+    // Pulisco il canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Itero per disegnarlo
+    for (let i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for (let j = 0; j < SCREEN_WIDTH; j++)
+        {
+            if (schermo[i][j])
+                ctx.fillRect(j, i, 1, 1)
+        }
+    }
+
+    animationID = window.requestAnimationFrame(DisegnaSchermo);
+}
+
+// Init function to get the canvas
+function init(){
+    canvas = document.getElementById("screen")
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    // Ingrandiso
+    ctx.scale(10,10);
 }
